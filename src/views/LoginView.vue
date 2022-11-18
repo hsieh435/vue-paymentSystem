c21bb0e57b24dbb228996931b7fc87260dc986b9
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-11-18 21:38:28 +0800

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,64 @@
 <template>
   <NavbarFun />
-  <h2 class="welcome">{{ msg.username }} 泥好</h2>
+  <h2 class="welcome">{{ msg.username }} 你好</h2>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from "vue";
-import NavbarFun from "../components/NavbarFun.vue";
+// import NavbarFun from "../components/NavbarFun.vue";
 import "bootstrap/dist/css/bootstrap.css";
-// import { assertWhileStatement } from "@babel/types";
+import axios from "axios";
 export default defineComponent({
   name: "LoginView",
   components: {
-    NavbarFun,
+    // NavbarFun,
   },
 
   setup() {
-    // console.log("a:", localStorage.getItem("username"));
+    const username = localStorage.getItem("username");
+    const userToken = {
+      token: localStorage.getItem("userJWT"),
+    };
+
+    const postheader = {
+      header: {
+        Authorization: "Bearer " + userToken.token,
+        // Bearer 跟 token 中間有一個空格
+      },
+    };
+    console.log("b:", "Bearer " + userToken.token);
+    // console.log("f:", postheader);
+    axios
+      .post(
+        "http://localhost:8085/paymentSystem/api/PSUser/findPSUserByNotesId",
+        { data: "" },
+        {
+          headers: {
+            Authorization: "Bearer " + userToken.token,
+            // Bearer 跟 token 中間有一個空格
+          },
+        }
+        // notesId: username,
+      )
+
+      .then((response) => {
+        // if(userToken.jwt ==){
+        // }
+        // alert("c:");
+      })
+
+      .catch((error) => {
+        console.log("發生錯誤");
+      });
+
+    // console.log("a:", localStorage.getItem("userToken"));
+
     const msg = reactive({
-      username: localStorage.getItem("username")
+      username: localStorage.getItem("username"),
     });
-    // console.log("b:", msg.username)
+
+    // response.data.userInfo.userName
+
     return {
       msg,
     };
@@ -33,5 +72,9 @@ export default defineComponent({
   margin-top: 200px;
   margin-left: 50px;
   color: black;
+}
+
+nav {
+  padding-top: 0px;
 }
 </style>
