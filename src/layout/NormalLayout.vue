5ec543019a66755e758267ff7eee980c2d6df763
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-11-18 21:35:50 +0800

    Add files via upload

---
+++ src/layout/NormalLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <router-link to="/home">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/">LoginPage</router-link> |
+    <router-link to="/loginpage2">LoginPage2</router-link> |
+    <router-link to="/loginpage3">LoginPage3</router-link> |
+    <router-link to="/LoadingEffect">LoadingEffect</router-link>
+    <NavbarFun />
+  </nav>
+  <router-view />
+</template>
+
+<script lang="ts">
+import NavbarFun from "../components/NavbarFun.vue";
+export default {
+  name: "NormalLayout",
+  components: {
+    NavbarFun,
+  },
+};
+</script>
+
+<style lang="scss">
+nav {
+  padding-top: 30px;
+  z-index: 10;
+  /* outline:1px solid black; */
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: rgb(46, 189, 89);
+    }
+  }
+}
+</style>
