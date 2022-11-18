00f02c483e27017934162864f962df44832f3693
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-11-18 21:37:07 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,46 +1,85 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NormalLayout from "../layout/NormalLayout.vue"
+import LoginLayout from "../layout/LoginLayout.vue"
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/home',
     name: 'home',
-    component: HomeView
+    component: NormalLayout,
+    children:[
+      {
+        path:"",
+        component:HomeView,
+      }
+    ]
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import('../views/AboutView.vue')
+    component: NormalLayout,
+    children:[
+      {
+        path:"",
+        component: () => import('../views/AboutView.vue')
+      }
+    ]
   },
   {
     path: '/',
     name: 'loginpage',
-    component: () => import("../views/LoginPage.vue")
+    component: LoginLayout,
+    children:[
+      {
+        path:"",
+        component: () => import('../views/LoginPage.vue')
+      }
+    ]
   },
   {
     path: '/loginpage2',
     name: 'loginpage2',
-    component: () => import("../views/LoginPage2.vue")
+    component: NormalLayout,
+    children:[
+      {
+        path:"",
+        component: () => import('../views/LoginPage2.vue')
+      }
+    ]
   },
   {
     path: '/loginpage3',
     name: 'loginpage3',
-    component: () => import("../views/LoginPage3.vue")
+    component: NormalLayout,
+    children:[
+      {
+        path:"",
+        component: () => import('../views/LoginPage3.vue')
+      }
+    ]
   },
   {
     path: '/LoadingEffect',
     name: 'LoadingEffect',
-    component: () => import("../views/LoadingEffect.vue")
+    component: NormalLayout,
+    children:[
+      {
+        path:"",
+        component: () => import('../views/LoginPage3.vue')
+      }
+    ]
   },
-  // {
-  //   path: '/login/LoginView',
-  //   name: 'LoginView',
-  //   component: () => import("../views/LoginView.vue"),
-  // },
   {
     path: '/LoginView',
     name: 'LoginView',
-    component: () => import("../views/LoginView.vue")
+    component: NormalLayout,
+    children:[
+      {
+        path:"",
+        component: () => import('../views/LoginView.vue')
+      }
+    ]
   },
 ]
 
