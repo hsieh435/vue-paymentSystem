64daa4ae21f9fa4e85e6c4314531e4516644fc6f
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-11-21 13:32:34 +0800

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,63 +1,75 @@
+<!-- 登入後的畫面 -->
 <template>
   <NavbarFun />
-  <h2 class="welcome">{{ msg.username }} 你好</h2>
+  <h2 class="welcome">
+    {{ msg.userName }} 你好 <br />
+    USERNAME：{{ msg.userID }} <br />
+    部門：{{ msg.userDepartment }} <br />
+    職級：{{ msg.userClass }}
+  </h2>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-// import NavbarFun from "../components/NavbarFun.vue";
+import { defineComponent, reactive, onMounted } from "vue";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 export default defineComponent({
   name: "LoginView",
-  components: {
-    // NavbarFun,
-  },
 
   setup() {
-    const username = localStorage.getItem("username");
-    const userToken = {
-      token: localStorage.getItem("userJWT"),
-    };
+    const userId = localStorage.getItem("userId");
+    const token = localStorage.getItem("userJWT");
+    // console.log("b1:", token);
+    // console.log("b2:", userId);
 
-    const postheader = {
-      header: {
-        Authorization: "Bearer " + userToken.token,
-        // Bearer 跟 token 中間有一個空格
-      },
-    };
-    console.log("b:", "Bearer " + userToken.token);
-    // console.log("f:", postheader);
-    axios
-      .post(
-        "http://localhost:8085/paymentSystem/api/PSUser/findPSUserByNotesId",
-        { data: "" },
-        {
-          headers: {
-            Authorization: "Bearer " + userToken.token,
-            // Bearer 跟 token 中間有一個空格
-          },
-        }
-        // notesId: username,
-      )
+    onMounted(() => {
+      axios
+        .post(
+          "http://localhost:8085/paymentSystem/api/PSUser/findPSUserByNotesId",
+          { notesId: userId },
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+              // Bearer 跟 token 中間有一個空格
+            },
+          }
+        )
+        // post 放三個參數，url、data、config(header)
 
-      .then((response) => {
-        // if(userToken.jwt ==){
-        // }
-        // alert("c:");
-      })
+        .then((response) => {
+          // console.log("c1:", response.data);
+          msg.userName = response.data.data.userInfo.userName;
+          msg.userID = response.data.data.userInfo.notesId;
+          msg.userDepartment = response.data.data.userInfo.userOrganization;
+          msg.userClass = response.data.data.userInfo.userTitle;
+        })
 
-      .catch((error) => {
-        console.log("發生錯誤");
-      });
+        .catch((error) => {
+          console.log("發生錯誤");
+        });
 
-    // console.log("a:", localStorage.getItem("userToken"));
+      // 分隔線，以下為權限查詢
 
-    const msg = reactive({
-      username: localStorage.getItem("username"),
+      axios
+        .post(
+          "http://localhost:8085/paymentSystem/api/functionGroup/findAllFunctionGroupWhereFunctionIdInPermission",
+          { token: token }
+        )
+        .then((response) => {
+          console.log("123")
+        })
+
+        .catch((error) => {
+          console.log("權限查詢發生錯誤");
+        });
     });
 
-    // response.data.userInfo.userName
+    const msg = reactive({
+      userName: "",
+      userID: "",
+      userDepartment: "",
+      userClass: "",
+    });
 
     return {
       msg,
@@ -69,8 +81,8 @@ export default defineComponent({
 <style>
 .welcome {
   text-align: left;
-  margin-top: 200px;
-  margin-left: 50px;
+  margin-top: 100px;
+  margin-left: 20px;
   color: black;
 }
 
