caa377731cfca9b388ffa499abc98a784f806d15
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-11-25 12:15:50 +0800

    Update backToLoginPage.vue

--- src/components/backToLoginPage.vue
+++ src/components/backToLoginPage.vue
@@ -1,16 +1,41 @@
 <template>
-  <!-- <button @click="back()">返回主頁面</button> -->
-  <button to="/LoginView">返回主頁面</button>
+  <button class="back" @click="back()">返回主頁面</button>
+  <!-- <button to="/LoginView">返回主頁面</button> -->
 </template>
 
 <script lang="ts">
+import { useRoute, useRouter } from "vue-router";
 export default {
-  name:"backToLoginPage",
+  name: "backToLoginPage",
 
-  // function back() {
-    
-  // }
-}
+  setup() {
+    const router = useRouter();
+    function back() {
+      router.push("./LoginView");
+    }
+    return {
+      back,
+    };
+  },
+};
 </script>
 
-<!-- backToLoginPage -->
+<style>
+.back {
+  height: 50px;
+  width: 300px;
+  border-radius: 25px;
+  margin: 50px 0px 0px 0px;
+  border: 0px;
+  font-size: 24px;
+  color: rgb(0, 0, 0);
+  transition: 0.3s;
+}
+
+.back:hover {
+  background-color: rgb(46, 189, 89);
+  box-shadow: 10px 10px 10px 0px #000000;
+  border: 1px solid rgb(46, 47, 51);
+  color: rgb(255, 255, 255);
+}
+</style>
