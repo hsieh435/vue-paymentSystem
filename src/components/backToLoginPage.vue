731e4aa8f3117865fdcca899891b9fa2cb0763b1
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-11-25 12:14:51 +0800

    Add files via upload

---
+++ src/components/backToLoginPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <button @click="back()">返回主頁面</button> -->
+  <button to="/LoginView">返回主頁面</button>
+</template>
+
+<script lang="ts">
+export default {
+  name:"backToLoginPage",
+
+  // function back() {
+    
+  // }
+}
+</script>
+
+<!-- backToLoginPage -->
