54cf0f8ef922e436bbebdd62f1d5b052378a61ca
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-12-06 09:58:06 +0800

    Add files via upload

---
+++ src/ChildInfo4.vue
@@ -0,0 +1,22 @@
+<!-- 動態插槽 -->
+<template>
+  <p v-for="area in areas" :key="area">
+    <slot :name="area"></slot>
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+export default defineComponent({
+  name: "ChildInfo4",
+
+  setup() {
+    const areas = ["家裡", "公司", "學校"];
+    return {
+      areas,
+    };
+  },
+});
+</script>
+
+<style></style>
