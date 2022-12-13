213b823bb9068dffb19557c1bb69cdc75d8a60c8
hsieh435 <106055247+hsieh435@users.noreply.github.com>
2022-12-13 11:48:55 +0800

    Update RoleManagement.vue

--- src/views/subfunction/managerGroup/RoleManagement.vue
+++ src/views/subfunction/managerGroup/RoleManagement.vue
@@ -2,10 +2,10 @@
 <template>
   <h1 class="subfunctiontitle">角色與權限管理</h1>
   <AddNewRole></AddNewRole>
-  <UserList></UserList>
-  <AuthorityManagement v-if="adjustAuthority == null"></AuthorityManagement>
-  <EditRole v-if="adjustRole == null"></EditRole>
-  <DeleteRole v-if="deleteRole == null"></DeleteRole>
+  <RoleList></RoleList>
+  <!-- <AuthorityManagement v-if="adjustAuthority == null"></AuthorityManagement> -->
+  <!-- <EditRole v-if="adjustRole == null"></EditRole> -->
+  <!-- <DeleteTheRole v-if="deleteRole == null"></DeleteTheRole> -->
   <BackToLoginPage></BackToLoginPage>
 </template>
 
@@ -13,43 +13,43 @@
 import { defineComponent, ref, provide, inject } from "vue";
 import "@/views/subfunction/subfunction.css";
 import AddNewRole from "../../../components/subfunccomponents/aboutrolemanagement/AddNewRole.vue";
-import UserList from "../../../components/subfunccomponents/aboutrolemanagement/UserList.vue";
-import AuthorityManagement from "../../../components/subfunccomponents/aboutrolemanagement/AuthorityManagement.vue";
-import EditRole from "../../../components/subfunccomponents/aboutrolemanagement/EditRole.vue";
-import DeleteRole from "../../../components/subfunccomponents/aboutrolemanagement/DeleteRole.vue";
+import RoleList from "../../../components/subfunccomponents/aboutrolemanagement/RoleList.vue";
+// import AuthorityManagement from "../../../components/subfunccomponents/aboutrolemanagement/AuthorityManagement.vue";
+// import EditRole from "../../../components/subfunccomponents/aboutrolemanagement/EditRole.vue";
+// import DeleteTheRole from "../../../components/subfunccomponents/aboutrolemanagement/DeleteTheRole.vue";
 import BackToLoginPage from "../../../components/BackToLoginPage.vue";
 export default defineComponent({
   name: "RoleManagement",
   components: {
     BackToLoginPage,
     AddNewRole,
-    UserList,
-    AuthorityManagement,
-    EditRole,
-    DeleteRole,
+    RoleList,
+    // AuthorityManagement,
+    // EditRole,
+    // DeleteTheRole,
   },
   props: {},
   setup() {
     // 修改權限功能的 value 值，控制出現與否
-    const adjustAuthority = ref();
-    adjustAuthority.value = true;
+    // const adjustAuthority = ref();
+    // adjustAuthority.value = true;
 
     // 編輯角色功能的 value 值，控制出現與否
-    const adjustRole = ref();
-    adjustRole.value = true;
+    // const adjustRole = ref();
+    // adjustRole.value = true;
 
     // 刪除角色功能的 value 值，控制出現與否
-    const deleteRole = ref();
-    deleteRole.value = true;
+    // const deleteRole = ref();
+    // deleteRole.value = true;
 
-    provide("adjustAuthority", adjustAuthority);
-    provide("adjustRole", adjustRole);
-    provide("deleteRole", deleteRole);
+    // provide("adjustAuthority", adjustAuthority);
+    // provide("adjustRole", adjustRole);
+    // provide("deleteRole", deleteRole);
 
     return {
-      adjustAuthority,
-      adjustRole,
-      deleteRole,
+      // adjustAuthority,
+      // adjustRole,
+      // deleteRole,
     };
   },
 });
